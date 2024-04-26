c1fedb34ccb37e2d50f2b1c513590bb98d86599f
Dang Vu <vnhd1995@gmail.com>
2024-04-26 18:52:19 +0700

    update for vs code

--- nvim/lua/vnhd/plugins/tree.lua
+++ nvim/lua/vnhd/plugins/tree.lua
@@ -8,9 +8,18 @@ return {
   config = function()
     require('nvim-tree').setup {
       view = { adaptive_size = true },
+      filters = {
+        dotfiles = false,
+        git_clean = false,
+        no_buffer = false,
+        custom = { '__pycache__' },
+      },
+      update_focused_file = {
+        enable = true,
+        update_cwd = false,
+      },
     }
 
     vim.api.nvim_set_keymap('n', '<C-e>', '<cmd>:NvimTreeToggle<CR>', { noremap = true })
   end,
 }
-
