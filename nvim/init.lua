14e886cba60dce8b08ae20ef410f636f8493b882
Dang Vu <vnhd1995@gmail.com>
2024-05-07 11:46:03 +0700

    update loading for vscode dynamically

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,11 @@
-require 'vnhd.core'
-require 'vnhd.key-maps'
-require 'vnhd.lazy'
+
+
+if vim.g.vscode then
+    -- VSCode extension
+    require 'vnhd.vscode'
+else
+    -- ordinary Neovim
+    require 'vnhd.core'
+    require 'vnhd.key-maps'
+    require 'vnhd.lazy'
+end
