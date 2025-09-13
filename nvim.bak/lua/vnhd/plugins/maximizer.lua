c60791d5a9845df7abfbcd18c14994877751d7f2
Dang Vu <vnhd1995@gmail.com>
2025-09-13 11:26:54 +0700

    update keypress cfg, rm nvim.bak, zed

--- nvim.bak/lua/vnhd/plugins/maximizer.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'szw/vim-maximizer',
-  keys = {
-    { '<leader>mm', '<cmd>MaximizerToggle<CR>', desc = '[M]aximize/[m]inimize a split' },
-  },
-}
--- nvim.bak/lua/vnhd/python.lua
+++
@@ -1,6 +0,0 @@
--- Disable the default Python 3 provider
-vim.g.loaded_python3_provider = 0
-
--- Specify the path to the Python 3 interpreter
--- global install pip neovim to global python interpreter
-vim.g.python3_host_prog = '~/.pyenv/shims/python'
