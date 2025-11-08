ade4a9b954535958203a8d2f4dfaec47290ac5fe
Dang Vu <vnhd1995@gmail.com>
2025-11-08 09:45:17 +0700

    config signcolumn

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -7,6 +7,9 @@ local opt = vim.opt
 -- Line numbers
 opt.relativenumber = true
 
+-- Sign column (allow multiple signs: diagnostics, git, spell checker)
+opt.signcolumn = "yes:3"  -- always show 3 sign columns (fixed width, no flickering)
+
 -- UI customizations
 vim.api.nvim_set_hl(0, "Visual", { bg = "#404040", fg = "#ffffff" })
 
